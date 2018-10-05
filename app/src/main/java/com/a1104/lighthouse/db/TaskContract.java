c01886f9cda623122ff8d6821b7dd42fbd3a6ee4
Unknown <matasramanuaskas@gmail.com>
2018-10-05 15:00:34 +0300

    Template project finished

---
+++ app/src/main/java/com/a1104/lighthouse/db/TaskContract.java
@@ -0,0 +1,14 @@
+package com.a1104.lighthouse.db;
+
+import android.provider.BaseColumns;
+
+public class TaskContract {
+    public static final String DB_NAME = "com.a1104.lighthouse.db";
+    public static final int DB_VERSION = 1;
+
+    public class TaskEntry implements BaseColumns {
+        public static final String TABLE = "tasks";
+
+        public static final String COL_TASK_TITLE = "title";
+    }
+}
